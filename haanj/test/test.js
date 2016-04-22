a99bc37af18b5589240b04c35deb3a972b38a371
haanj <joshua.haan@ymail.com>
2016-04-22 12:56:18 -0700

    adds e2e testing

--- haanj/test/test.js
+++ haanj/test/test.js
@@ -3,7 +3,7 @@ describe('e2e testing for simple services app', function() {
   var firstButton = element(by.buttonText('This button increments'))
 
   var secondCount = element(by.binding('anotherctrl.count'))
-  var secondButton = element(by.binding('Get the updated value'))
+  var secondButton = element(by.buttonText('Get the updated value'))
 
   beforeEach(function() {
     browser.get('http://localhost:8080/') 
@@ -14,10 +14,40 @@ describe('e2e testing for simple services app', function() {
   })
 
   it('should have correct starting values', function() {
-    console.log(firstCount.getAttribute('value'))   
-
-    //expect(firstCount.getAttribute('value')).toEqual(0) 
-    //expect(secondCount.getAttribute('value')).toEqual(0) 
+    expect(firstCount.getText()).toEqual('0')
+    expect(secondCount.getText()).toEqual('0')
   })
 
+  it('should first should increment count when first button is pressed', function() {
+    firstButton.click()
+    expect(firstCount.getText()).toEqual('1')
+    expect(secondCount.getText()).toEqual('0')
+  
+    firstButton.click()
+    expect(firstCount.getText()).toEqual('2')
+    expect(secondCount.getText()).toEqual('0')
+
+    for(var i = 0; i < 100; i++) firstButton.click()
+    expect(firstCount.getText()).toEqual('102')
+    expect(secondCount.getText()).toEqual('0')
+  })
+
+  it('should update second count when second button is pressed', function() {
+    for(var i = 0; i < 50; i++) firstButton.click()
+    expect(firstCount.getText()).toEqual('50')
+    expect(secondCount.getText()).toEqual('0')
+
+    secondButton.click()
+    expect(firstCount.getText()).toEqual('50')
+    expect(secondCount.getText()).toEqual('50')
+    
+  })
+
+
+
+
+
+
+    
+  
 })
